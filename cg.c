f7bd599a3055dc43b6cf0f1153527b5beabc1605
kswirydo <kasia.swirydowicz@gmail.com>
2024-02-02 13:02:31 -0800

    formatting ...

--- cg.c
+++ cg.c
@@ -17,109 +17,109 @@ void cg(int n, double nnz,
        ){
 
 #if (CUDA || HIP)
-	double *r;
-	double *w;
-	double *p;
-	double *q;
+  double *r;
+  double *w;
+  double *p;
+  double *q;
 
-	r = (double*) mallocForDevice(r, n, sizeof(double));
-	w = (double*) mallocForDevice(w, n, sizeof(double));
-	p = (double*) mallocForDevice(p, n, sizeof(double));
-	q = (double*) mallocForDevice(q, n, sizeof(double));
+  r = (double*) mallocForDevice(r, n, sizeof(double));
+  w = (double*) mallocForDevice(w, n, sizeof(double));
+  p = (double*) mallocForDevice(p, n, sizeof(double));
+  q = (double*) mallocForDevice(q, n, sizeof(double));
 #else
-	double *r = (double *) calloc (n, sizeof(double));
-	double *w = (double *) calloc (n, sizeof(double));
-	double *p = (double *) calloc (n, sizeof(double));
-	double *q = (double *) calloc (n, sizeof(double));
+  double *r = (double *) calloc (n, sizeof(double));
+  double *w = (double *) calloc (n, sizeof(double));
+  double *p = (double *) calloc (n, sizeof(double));
+  double *q = (double *) calloc (n, sizeof(double));
 #endif
 
-	double alpha, beta, tolrel, rho_current, rho_previous, pTq;
-	double one = 1.0;
-	double zero = 0.0;  
-	int notconv = 1, iter = 0;
-	//compute initial norm of r
-	/* r = A*x */
-	csr_matvec(n, nnz, ia, ja, a, x, r, &one, &zero, "A");
-	
+  double alpha, beta, tolrel, rho_current, rho_previous, pTq;
+  double one = 1.0;
+  double zero = 0.0;  
+  int notconv = 1, iter = 0;
+  //compute initial norm of r
+  /* r = A*x */
+  csr_matvec(n, nnz, ia, ja, a, x, r, &one, &zero, "A");
+
   /* r = -b +r = Ax-b */
-	axpy(n, -1.0, b, r);
-	printf("Norm of r %e \n", dot(n, r,r));  
-	
+  axpy(n, -1.0, b, r);
+  printf("Norm of r %e \n", dot(n, r,r));  
+
   /* r=(-1.0)*r */
-	scal(n, -1.0, r);
-	
+  scal(n, -1.0, r);
+
   /* norm of r */
-	res_norm_history[0] = dot(n, r,r);
-	res_norm_history[0] = sqrt(res_norm_history[0]);
-	tolrel = tol * res_norm_history[0];
+  res_norm_history[0] = dot(n, r,r);
+  res_norm_history[0] = sqrt(res_norm_history[0]);
+  tolrel = tol * res_norm_history[0];
 
   printf("CG: it %d, res norm %5.5e \n",0, res_norm_history[0]);
-	
+
   while (notconv){
-    //	printf("Norm of X before prec %16.16e \n", dot(n, r,r));  
-		prec_function(ia, ja, a, nnz, prec_data, r, w);
+    //  printf("Norm of X before prec %16.16e \n", dot(n, r,r));  
+    prec_function(ia, ja, a, nnz, prec_data, r, w);
     // printf("Norm of X after prec %16.16e \n", dot(n, w,w));  
-		
-    /* rho_current = r'*w; */
-		rho_current = dot(n, r, w);
-		if (iter == 0) {
-			vec_copy(n, w, p);
-		} else {
-			beta = rho_current/rho_previous;
+
+   /* rho_current = r'*w; */
+    rho_current = dot(n, r, w);
+    if (iter == 0) {
+      vec_copy(n, w, p);
+    } else {
+      beta = rho_current/rho_previous;
       // printf("scaling by beta = %5.5e, rho_current = %5.5e, rho_previous = %5.5e \n", beta, rho_current, rho_previous);
       /* p = w+bet*p; */
-			
+
       scal(n, beta, p);
-			axpy(n, 1.0, w, p);
-		}
-		/* q = As*p; */
-		csr_matvec(n, nnz, ia, ja, a, p, q, &one, &zero, "A");
-		
+      axpy(n, 1.0, w, p);
+    }
+    /* q = As*p; */
+    csr_matvec(n, nnz, ia, ja, a, p, q, &one, &zero, "A");
+
     /* alpha = rho_current/(p'*q);*/
     // printf("p'*p = %16.16e, q'*q = %16.16f \n",dot(n, p,p), dot(n, q,q) );
 
-		pTq = dot(n, p, q);
-		alpha = rho_current / pTq; 
-	  // printf("p^Tq = %5.15e,rho_current = %5.15e, alpha = %5.15e \n", pTq, rho_current, alpha);
-		
-    /* x = x + alph*p; */
-		axpy(n, alpha, p, x);
+    pTq = dot(n, p, q);
+    alpha = rho_current / pTq; 
+    // printf("p^Tq = %5.15e,rho_current = %5.15e, alpha = %5.15e \n", pTq, rho_current, alpha);
 
-		/* r = r - alph*q; */
-		axpy(n, (-1.0) * alpha, q, r );
-		
+    /* x = x + alph*p; */
+    axpy(n, alpha, p, x);
+
+    /* r = r - alph*q; */
+    axpy(n, (-1.0) * alpha, q, r );
+
     /* norm of r */
-		iter++;
-		res_norm_history[iter] = dot(n, r,r);
-		res_norm_history[iter] = sqrt(res_norm_history[iter]);
-		printf("CG: it %d, res norm %5.5e \n",iter, res_norm_history[iter]);
-		
+    iter++;
+    res_norm_history[iter] = dot(n, r,r);
+    res_norm_history[iter] = sqrt(res_norm_history[iter]);
+    printf("CG: it %d, res norm %5.5e \n",iter, res_norm_history[iter]);
+
     /* check convergence */
-		if ((res_norm_history[iter]) < tolrel) {
-			*flag = 0;
-			notconv = 0;
-			*it = iter; 
-		} else {
-			if (iter > maxit){
-				*flag = 1;
-				notconv = 0;
-				*it = iter; 
-			}
-		}
-		rho_previous = rho_current;
-	}//while
+    if ((res_norm_history[iter]) < tolrel) {
+      *flag = 0;
+      notconv = 0;
+      *it = iter; 
+    } else {
+      if (iter > maxit){
+        *flag = 1;
+        notconv = 0;
+        *it = iter; 
+      }
+    }
+    rho_previous = rho_current;
+  }//while
 
 
 #if (CUDA || HIP)
-	freeDevice(r);
-	freeDevice(w);
-	freeDevice(p);
-	freeDevice(q);
+  freeDevice(r);
+  freeDevice(w);
+  freeDevice(p);
+  freeDevice(q);
 #else
-	free(r);
-	free(w);
-	free(p);
-	free(q);
+  free(r);
+  free(w);
+  free(p);
+  free(q);
 #endif
 } /* cg */
 
